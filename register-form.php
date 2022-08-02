2b2a7631b811efc6b23372ca63fb6ebe8895475b
Adzhan-2K <107012205+Adzhan-2K@users.noreply.github.com>
2022-08-03 01:03:32 +0800

    register form-demo(dahan)

--- register-form.php
+++ register-form.php
@@ -2,6 +2,11 @@
 include 'templates/header.php';
 ?>
 
+<h1 class="container mt-5">Demo cara untuk membuat register form</h1><br>
+
+<p class="container">
+Anda dapat lihat dengan teliti bagaimana saya menggunakan setiap "input type". </p>
+
 <pre style="background:lightblue; width:80%; margin-top:50px; margin-left:70px;"><blockquote>
     &lt;form action="/action_page.php"&gt;
         &lt;div class="container"&gt;
@@ -27,50 +32,26 @@ include 'templates/header.php';
 
         &lt;label for="psw"&gt;&lt;b&gt;Password&lt;/b&gt;&lt;/label&gt;&lt;br&gt;
         &lt;input type="text" placeholder="Enter Password" name="psw" id="psw" required&gt;&lt;br&gt;
+
+        &lt;label for="pnumber"&gt;Phone Number&lt;/label&gt;&lt;br&gt;
+        &lt;input type="tel" id="pnumber" name="quantity" min="1" max="10"&gt;&lt;br&gt;&lt;br&gt;
+
+        &lt;button type="submit" class="registerbtn">Register&lt;/button&gt;
+
+        &lt;/div>&gt;
+
+        &lt;div class="container signin"&gt;
+    &lt;p&gt;Already have an account? &lt;a href="#"&gt;Sign in&lt;/a&gt;&lt;/p&gt;
+  &lt;/div&gt;
+&lt;/form&gt;
+
 </blockquote>
 </pre>
 
-<form action="/action_page.php">
-  <div class="container">
-    <h1>Register</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr>
+<p class="container">
+Setiap input type memainkan peranan yang berlainan</p>
 
-    <label for="fname"><b>First name:<b></label><br>
-    <input type="text" id="fname" name="fname" value="First Name"><br>
-
-    <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname" value="Last Name"><br>
-
-    <p><b>Gender</b></p>
-
-    <input type="radio" id="male" name="gender" value="Male">
-    <label for="male">Male</label>
-    <input type="radio" id="female" name="gender" value="Female">
-    <label for="female">Female</label><br>
-
-
-    <label for="email"><b>Email</b></label><br>
-    <input type="text" placeholder="Enter Email" name="email" id="email" required><br>
-
-    <label for="psw"><b>Password</b></label><br>
-    <input type="password" placeholder="Enter Password" name="psw" id="psw" required><br><br>
-
-    <label for="pnumber">Phone Number</label><br>
-    <input type="tel" id="pnumber" name="quantity" min="1" max="10"><br><br>
-
-    <button type="submit" class="registerbtn">Register</button>
-
-    
-  </div>
-
-  
-  <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a></p>
-  </div>
-</form>
-
-<h3 class="container mt-5">Klik Link di bawah untuk lihat website code di atas.</h3>
+<h3 class="container mt-5">Klik Link di bawah untuk lihat output bagi code di atas.</h3>
 <a style="margin-left: 50px; padding-bottom: 100px; color:blueviolet;" class="container" href="nota/dahan/Demo-Dahan/Part-1-RegisterForm/index.html">Index.html</a>
 
 
